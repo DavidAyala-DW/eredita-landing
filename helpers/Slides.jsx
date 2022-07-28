f4711420756eb92ed74a4678c862d1627b16fa16
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-07-27 19:13:48 -0500

    first version 2

---
+++ helpers/Slides.jsx
@@ -0,0 +1,51 @@
+import Slide1 from "../public/Rectangle4314.png";
+import Slide2 from "../public/VPLE37.png";
+
+export const galleries = [
+
+  {
+    handle: "slide1",
+    slides:[
+      Slide1,
+      Slide2,
+      Slide1,
+      Slide2,
+      Slide1,
+    ]
+  },
+  
+  {
+    handle: "slide2",
+    slides:[
+      Slide2,
+      Slide1,
+      Slide2,
+      Slide1,
+    ]
+  },
+
+  {
+    handle: "slide3",
+    slides:[
+      Slide1,
+      Slide2,
+      Slide2,
+    ]
+  },
+
+  {
+    handle: "slide4",
+    slides:[
+      Slide2,
+      Slide1,
+    ]
+  },
+
+  {
+    handle: "slide5",
+    slides:[
+      Slide1,
+    ]
+  },
+
+]
---
+++ helpers/Variants.jsx
@@ -0,0 +1,39 @@
+import Product1 from "../public/1.png";
+import Product2 from "../public/2.png";
+import Product3 from "../public/3.png";
+import Product4 from "../public/4.png";
+import Product5 from "../public/5.png";
+
+export const allVariants = [
+
+  {
+    handle: "slide1",
+    image: Product1,
+    color: "Shiny Black"
+  },
+
+  {
+    handle: "slide2",
+    image: Product2,
+    color: "Shiny Black2"
+  },
+
+  {
+    handle: "slide3",
+    image: Product3,
+    color: "Shiny Black3"
+  },
+
+  {
+    handle: "slide4",
+    image: Product4,
+    color: "Shiny Black 4"
+  },
+
+  {
+    handle: "slide5",
+    image: Product5,
+    color: "Shiny Black 5"
+  },
+
+]
